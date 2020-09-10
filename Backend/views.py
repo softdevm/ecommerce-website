79bb75b67d581750ca27e67da832ae6c42c4aa5c
softdevm <michaelweaver275@gmail.com>
2020-09-10 15:10:52 +0300

    Add API for user create and list

--- Backend/views.py
+++ Backend/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+from rest_framework import generics
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
